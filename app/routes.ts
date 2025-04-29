b53364df6e85101bd9dee6ff68996f868e5252d0
nanuThilak <nanuthilak2@gmail.com>
2025-04-29 17:25:59 +0530

    routingcomplete

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,10 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, layout, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+    layout('routes/admin/adminLayout/admin.layout.tsx', [
+        route('dashboard', 'routes/admin/dashboard/dashboard.tsx'),
+        route('all-users', 'routes/admin/allUsers/all.users.tsx')
+
+
+    ]),
+] satisfies RouteConfig;
